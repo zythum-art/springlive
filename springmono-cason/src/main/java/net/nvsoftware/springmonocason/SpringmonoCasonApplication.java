0309a6a99ba5033a8a8f51bfb082dd785486e3ad
张宇天 <yutian@Yutian.attlocal.net>
2024-04-28 19:13:05 -0700

    springone setup

---
+++ springmono-cason/src/main/java/net/nvsoftware/springmonocason/SpringmonoCasonApplication.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.springmonocason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmonoCasonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmonoCasonApplication.class, args);
+	}
+
+}
---
+++ springmono-cason/src/test/java/net/nvsoftware/springmonocason/SpringmonoCasonApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.springmonocason;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmonoCasonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
