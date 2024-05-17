1b0ebc24cd2dbbe8a7ed01e08d8cf194135e0109
zythum-art <yzhan959@asu.edu>
2024-05-16 23:05:18 -0700

    init ProductService

---
+++ ProductService-cason/src/main/java/net/nvsoftware/ProductServicecason/ProductServiceCasonApplication.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.ProductServicecason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceCasonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceCasonApplication.class, args);
+	}
+
+}
