e1678426b66c8c6261ea8b45b1a6cf5a06025c44
zythum-art <yzhan959@asu.edu>
2024-05-21 15:37:09 -0700

    setup OrderService with Spring Web / Spring Data JPA / MySQL Driver / Cloud Bootstrap / Eureka Discovery Client / Lombok

---
+++ OrderService-cason/src/main/java/net/nvsoftware/OrderServicecason/OrderServiceCasonApplication.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.OrderServicecason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceCasonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceCasonApplication.class, args);
+	}
+
+}
---
+++ OrderService-cason/src/test/java/net/nvsoftware/OrderServicecason/OrderServiceCasonApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.OrderServicecason;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceCasonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
