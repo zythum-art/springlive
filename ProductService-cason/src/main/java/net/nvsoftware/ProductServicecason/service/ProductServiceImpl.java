3eb8b429239282174246b59829550a06279bc460
zythum-art <yzhan959@asu.edu>
2024-05-20 18:00:04 -0700

    intro log4j2 to enable local log

--- ProductService-cason/src/main/java/net/nvsoftware/ProductServicecason/service/ProductServiceImpl.java
+++ ProductService-cason/src/main/java/net/nvsoftware/ProductServicecason/service/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package net.nvsoftware.ProductServicecason.service;
 
+import lombok.extern.log4j.Log4j2;
 import net.nvsoftware.ProductServicecason.entity.ProductEntity;
 import net.nvsoftware.ProductServicecason.model.ProductRequest;
 import net.nvsoftware.ProductServicecason.model.ProductResponse;
@@ -9,26 +10,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Log4j2
 public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepository productRepository;
     @Override
     public long addProduct(ProductRequest productRequest) {
+        log.info("Start: ProductService addProduct");
         ProductEntity productEntity = ProductEntity.builder()
                 .name(productRequest.getName())
                 .price(productRequest.getPrice())
                 .quantity(productRequest.getQuantity())
                 .build();
         productRepository.save(productEntity);
+        log.info("End: ProductService addProduct");
         return productEntity.getId();
     }
 
     @Override
     public ProductResponse getById(long id) {
+        log.info("Start: ProductService getById");
         ProductEntity productEntity = productRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("ProductService getById Not Found with id : " + id));
         ProductResponse productResponse = new ProductResponse();
         BeanUtils.copyProperties(productEntity, productResponse);
+        log.info("End: ProductService getById" + productResponse);
         return productResponse;
     }
 }
