8777b0a37a36c3cfb608b564332759f12f80fc57
zythum-art <yzhan959@asu.edu>
2024-05-21 20:00:53 -0700

    For OrderService, service layer place order step1 save orderEntity

--- OrderService-cason/src/main/java/net/nvsoftware/OrderServicecason/model/OrderRequest.java
+++ OrderService-cason/src/main/java/net/nvsoftware/OrderServicecason/model/OrderRequest.java
@@ -1,5 +1,8 @@
 package net.nvsoftware.OrderServicecason.model;
 
+import lombok.Data;
+
+@Data
 public class OrderRequest {
     private long productId;
     private long quantity;
--- OrderService-cason/src/main/java/net/nvsoftware/OrderServicecason/service/OrderServiceImpl.java
+++ OrderService-cason/src/main/java/net/nvsoftware/OrderServicecason/service/OrderServiceImpl.java
@@ -1,10 +1,13 @@
 package net.nvsoftware.OrderServicecason.service;
 
 import lombok.extern.log4j.Log4j2;
+import net.nvsoftware.OrderServicecason.entity.OrderEntity;
 import net.nvsoftware.OrderServicecason.model.OrderRequest;
 import net.nvsoftware.OrderServicecason.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 @Log4j2
@@ -16,9 +19,18 @@ public class OrderServiceImpl implements OrderService {
     public long placeOrder(OrderRequest orderRequest) {
         log.info("Start: OrderService placeOrder");
         // use OrderService to create OrderEntity with status CREATED, ORM JPA save to database
+        OrderEntity orderEntity = OrderEntity.builder()
+                .productId(orderRequest.getProductId())
+                .quantity(orderRequest.getQuantity())
+                .totalAmount(orderRequest.getTotalAmount())
+                .orderDate(Instant.now())
+                .orderStatus("CREATED")
+                .build();
+        orderRepository.save(orderEntity);
+        log.info("Process: OrderService placeOrder save orderEntity with orderId: " + orderEntity.getId());
         // call ProductService to check product quantity, if ok, reduce it, else throw not enough
         // call PaymentService to charge, if Success, mark order PAID, else CANCELLED
         log.info("End: OrderService placeOrder");
-        return 0;
+        return orderEntity.getId();
     }
 }
