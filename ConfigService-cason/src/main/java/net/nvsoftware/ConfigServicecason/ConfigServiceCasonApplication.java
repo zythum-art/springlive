3929db68d26e86efb6f23e77dbe7d0c953b63337
zythum-art <yzhan959@asu.edu>
2024-05-21 23:54:30 -0700

    setup ConfigService

---
+++ ConfigService-cason/src/main/java/net/nvsoftware/ConfigServicecason/ConfigServiceCasonApplication.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.ConfigServicecason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceCasonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceCasonApplication.class, args);
+	}
+
+}
---
+++ ConfigService-cason/src/test/java/net/nvsoftware/ConfigServicecason/ConfigServiceCasonApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.ConfigServicecason;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceCasonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
