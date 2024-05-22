2f1a46ea723a9e42c30c0e51a5b90bab33a896cf
zythum-art <yzhan959@asu.edu>
2024-05-22 00:12:00 -0700

    config configService, setup github config url, link it, add eureka config locally, enableConfigServer

--- ConfigService-cason/src/main/java/net/nvsoftware/ConfigServicecason/ConfigServiceCasonApplication.java
+++ ConfigService-cason/src/main/java/net/nvsoftware/ConfigServicecason/ConfigServiceCasonApplication.java
@@ -2,8 +2,10 @@ package net.nvsoftware.ConfigServicecason;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServiceCasonApplication {
 
 	public static void main(String[] args) {
