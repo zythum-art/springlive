60ee7ef507847179beb43e0dbfa53d6f9ce73118
zythum-art <yzhan959@asu.edu>
2024-05-25 03:01:21 -0700

    setup Gateway with Cloud Bootstrap/Gateway/Zipkin with sleuth/Eureka Discovery Client/Lombok/Config Client/Spring Boot Actuator

---
+++ APIGateWay-cason/src/main/java/net/nvsoftware/APIGateWay_cason/ApiGateWayCasonApplication.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.APIGateWay_cason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateWayCasonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateWayCasonApplication.class, args);
+	}
+
+}
---
+++ APIGateWay-cason/src/test/java/net/nvsoftware/APIGateWay_cason/ApiGateWayCasonApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.APIGateWay_cason;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateWayCasonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
