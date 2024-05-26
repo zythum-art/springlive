118aa4645e5f03c7c9c8ccb6d01d0df6d702c990
zythum-art <yzhan959@asu.edu>
2024-05-25 18:07:15 -0700

    add CircuitBreaker to OrderSerice

--- OrderService-cason/src/main/java/net/nvsoftware/OrderServicecason/client/PaymentServiceFeignClient.java
+++ OrderService-cason/src/main/java/net/nvsoftware/OrderServicecason/client/PaymentServiceFeignClient.java
@@ -1,5 +1,6 @@
 package net.nvsoftware.OrderServicecason.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import net.nvsoftware.OrderServicecason.model.PaymentRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
@@ -8,7 +9,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name="PAYMENT-SERVICE/payment")
+@CircuitBreaker(name = "feignCall", fallbackMethod = "feignCallPaymentFallBack")
 public interface PaymentServiceFeignClient {
     @PostMapping("/doPayment")
     public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
+
+    default void feignCallPaymentFallBack(Exception e) {
+        throw new RuntimeException("PAYMENT-SERVICE is down");
+    }
 }
--- OrderService-cason/src/main/java/net/nvsoftware/OrderServicecason/client/ProductServiceFeignClient.java
+++ OrderService-cason/src/main/java/net/nvsoftware/OrderServicecason/client/ProductServiceFeignClient.java
@@ -1,5 +1,6 @@
 package net.nvsoftware.OrderServicecason.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,7 +8,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name="PRODUCT-SERVICE/product")
+@CircuitBreaker(name = "feignCall", fallbackMethod = "feignCallPaymentFallBack")
 public interface ProductServiceFeignClient {
     @PutMapping("/reduceQuantity")
     public ResponseEntity<Void> reduceQuantity(@RequestParam long id, @RequestParam long quantity);
+
+    default void feignCallPaymentFallBack(Exception e) {
+        throw new RuntimeException("PRODUCT-SERVICE is down");
+    }
 }
