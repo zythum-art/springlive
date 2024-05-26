9faf7812b4a510328a1aa17318cedf0bd5896552
zythum-art <yzhan959@asu.edu>
2024-05-25 23:59:29 -0700

    setup and config okata 2OAuth, configuration okta SecurityWebFilterChain to filter anyExchange protect 401

--- APIGateWay-cason/src/main/java/net/nvsoftware/APIGateWay_cason/ApiGateWayCasonApplication.java
+++ APIGateWay-cason/src/main/java/net/nvsoftware/APIGateWay_cason/ApiGateWayCasonApplication.java
@@ -2,6 +2,10 @@ package net.nvsoftware.APIGateWay_cason;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.keyvalue.core.mapping.KeySpaceResolver;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ApiGatewayCasonApplication {
@@ -10,4 +14,8 @@ public class ApiGatewayCasonApplication {
 		SpringApplication.run(ApiGatewayCasonApplication.class, args);
 	}
 
+	@Bean
+	KeyResolver userIdSolver() {
+		return exchange -> Mono.just("userId");
+	}
 }
