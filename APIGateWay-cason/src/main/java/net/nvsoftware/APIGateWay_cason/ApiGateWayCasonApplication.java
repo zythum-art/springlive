ec09e20842bada11172caced611fc1a1f3cff706
zythum-art <yzhan959@asu.edu>
2024-05-26 00:03:39 -0700

    Revert "setup and config okata 2OAuth, configuration okta SecurityWebFilterChain to filter anyExchange protect 401"
    This reverts commit 9faf7812b4a510328a1aa17318cedf0bd5896552.

--- APIGateWay-cason/src/main/java/net/nvsoftware/APIGateWay_cason/ApiGateWayCasonApplication.java
+++ APIGateWay-cason/src/main/java/net/nvsoftware/APIGateWay_cason/ApiGateWayCasonApplication.java
@@ -2,10 +2,6 @@ package net.nvsoftware.APIGateWay_cason;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.keyvalue.core.mapping.KeySpaceResolver;
-import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ApiGatewayCasonApplication {
@@ -14,8 +10,4 @@ public class ApiGatewayCasonApplication {
 		SpringApplication.run(ApiGatewayCasonApplication.class, args);
 	}
 
-	@Bean
-	KeyResolver userIdSolver() {
-		return exchange -> Mono.just("userId");
-	}
 }
